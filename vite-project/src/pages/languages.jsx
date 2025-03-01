d89840af431a4a2f4f2483685052a8092c15b387
Malik <malik.h.jacobs@outlook.com>
2025-03-01 11:24:50 -0500

    added divs for languages

--- vite-project/src/pages/languages.jsx
+++ vite-project/src/pages/languages.jsx
@@ -1,6 +1,16 @@
 const Languages = () => {
     return(<>
     <h1>Languages</h1>
+    <div className="language" style={{ "--boxColor": "rgb(0, 255, 225)" }}>
+        <h2>Python</h2>
+        <h4>description</h4>
+        <button>learn more</button>
+    </div>
+    <div className="language" style={{ "--boxColor": "rgb(26, 226, 59)" }}>
+        <h2>Java</h2>
+        <h4>description</h4>
+        <button>learn more</button>
+    </div>
     </>);
 }
 
