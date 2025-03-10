d73bb2ae4a385747c1a48b9c26ef1c1dbf054af2
Malik <malik.h.jacobs@outlook.com>
2025-03-09 23:33:32 -0400

    added switch mode button to languages

--- vite-project/src/pages/languages.jsx
+++ vite-project/src/pages/languages.jsx
@@ -1,13 +1,22 @@
 const Languages = () => {
     return(<>
+    <div>
     <h1>Languages</h1>
-    <div className="language" style={{ "--boxColor": "rgb(0, 255, 225)" }}>
+    <button>switch mode</button>
+    </div>
+    
+    <div className="language" style={{ "--boxColor": "rgb(0, 255, 187)" }}>
         <h2>Python</h2>
         <h4>description</h4>
         <button>learn more</button>
     </div>
-    <div className="language" style={{ "--boxColor": "rgb(26, 226, 59)" }}>
+    <div className="language" style={{ "--boxColor": "rgb(251, 197, 5)" }}>
         <h2>Java</h2>
+        <h4>description</h4>
+        <button>learn more</button>
+    </div>
+    <div className="language" style={{ "--boxColor": "rgb(43, 227, 27)" }}>
+        <h2>Javascript</h2>
         <h4>description</h4>
         <button>learn more</button>
     </div>
