4aa337833c978a7a584f693b341e03929c462bfc
Malik <malik.h.jacobs@outlook.com>
2025-03-23 12:05:41 -0400

    added category buttons

--- vite-project/src/pages/tools.jsx
+++ vite-project/src/pages/tools.jsx
@@ -1,6 +1,12 @@
 const Tools = () => {
     return(<>
     <h1>Tools</h1>
+    <div>
+        <h2>Select:</h2>
+        <button>Frameworks</button>
+        <button>Development Tools</button>
+        <button>Guides/Tutorials</button>
+    </div>
     </>);
 }
 
