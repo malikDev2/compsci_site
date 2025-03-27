c9cc650bbab6c0b291f0fad703e6a7b343dbdfcf
Malik <malik.h.jacobs@outlook.com>
2025-03-27 13:18:55 -0400

    added categories to devtools

--- vite-project/src/pages/devtools.jsx
+++ vite-project/src/pages/devtools.jsx
@@ -3,6 +3,12 @@ import { Link } from "react-router-dom";
 const Devtools = () =>{
     return(<>
     <h1>Development Tools</h1>
+    <div className="category">
+        <h2>Category:</h2>
+        <button>Code Editor</button>
+        <button>Aritifical Intelligence</button>
+        <button>Version Control</button>
+    </div>
     <Link to="/tools">
         <button>Back</button>
     </Link>
