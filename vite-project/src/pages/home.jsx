d2efc0e2619ac7b57c18d0618a4b52bd08985d75
Malik <malik.h.jacobs@outlook.com>
2025-03-29 23:26:22 -0400

    created divs on home page

--- vite-project/src/pages/home.jsx
+++ vite-project/src/pages/home.jsx
@@ -1,6 +1,33 @@
+import { Link } from "react-router-dom";
+
 const Home = () =>{
     return <>
     <h1>Home page for code site</h1>
+    <div className="home">
+        <div className="homediv">
+            <h1>Courses</h1>
+            <h2>description</h2>
+            <Link to="courses">
+                <button>Visit</button>
+            </Link>
+        </div>
+        <div className="homediv">
+            <h1>Programming Languages</h1>
+            <h2>description</h2>
+            <Link to="languages">
+                <button>Visit</button>
+            </Link>
+            
+        </div>
+        <div className="homediv">
+            <h1>Tools</h1>
+            <h2>description</h2>
+            <Link to="tools">
+                <button>Visit</button>
+            </Link>
+            
+        </div>
+    </div>
     </>;
 }
 export default Home;
