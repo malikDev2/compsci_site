05bf343cd03d08a6a6464ce449183d57b9abd6ed
Malik <malik.h.jacobs@outlook.com>
2025-03-31 15:16:12 -0400

    created descriptions for cs courses

--- vite-project/src/pages/cs.jsx
+++ vite-project/src/pages/cs.jsx
@@ -6,8 +6,8 @@ const Cs = () => {
     const courses = [
         {name: 'AP Computer Science A (Intro to Computer Science)', type: 'programming', color: 'rgb(77, 208, 255)', body: 'Yeah'},
         {name: 'AP Computer Science Principales', type: 'programming', color: 'rgb(55, 251, 37)', body: ''},
-        {name: 'Data Structures', type: 'programming', color: 'rgb(248, 62, 29)', body: 'Data structures...'},
-        {name: 'Computer Architecture', type: 'programming', color: 'rgb(23, 93, 204)', body: 'C and Assembly and Memory'},
+        {name: 'Data Structures', type: 'programming', color: 'rgb(248, 62, 29)', body: 'Introduces data structures such as linked lists, trees, and hash tables. Also expands on time complexity, sorting, and other topics.'},
+        {name: 'Computer Architecture', type: 'programming', color: 'rgb(23, 93, 204)', body: 'Introduces students to C and Assembly to understand computing at a lower level. Includes topics such as hardware, memory, compilation, and more.'},
         {name: 'Intro to Discrete Structures I', type: 'theory', color: 'rgb(255, 213, 77)', body: ''}
     ];
 
