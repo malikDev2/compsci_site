88b8e78b96f764aa6359237d5e0dab9cd8f18fe1
Malik <malik.h.jacobs@outlook.com>
2025-04-06 12:43:21 -0400

    turned tools buttons into divs

--- vite-project/src/pages/tools.jsx
+++ vite-project/src/pages/tools.jsx
@@ -3,18 +3,25 @@ import { Link } from "react-router-dom";
 const Tools = () => {
     return(<>
     <h1>Tools</h1>
-    <div>
-        <h2>Select:</h2>
-        <Link to="frameworks">
-            <button>Frameworks/Environments</button>
+    <div className="home">
+        <Link to="frameworks" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div className="homediv">
+                <h1>Frameworks</h1>
+                <h4>Frames</h4>
+            </div>
         </Link>
-        <Link to="devtools">
-            <button>Development Tools</button>
+        <Link to="devtools" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div className="homediv" style={{backgroundColor: 'rgb(12,20,120)'}}>
+                <h1>Devtools</h1>
+                <h4>dev</h4>
+            </div>
         </Link>
-        <Link to="resources">
-            <button>Resources</button>
-        </Link>
-        
+        <Link to="resources" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div className="homediv">
+                <h1>Resources</h1>
+                <h4>helpful</h4>
+            </div>
+        </Link>  
     </div>
     </>);
 }
