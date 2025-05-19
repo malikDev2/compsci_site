ab57fcba2fc166aab7de40e54d6387f6cc34f6ce
Malik <malik.h.jacobs@outlook.com>
2025-05-18 21:16:52 -0400

    added images to course divs

--- vite-project/src/pages/courses.jsx
+++ vite-project/src/pages/courses.jsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import mathLogo from '../assets/mathlogo.png';
+import csLogo from '../assets/cslogo.png';
+import dsLogo from '../assets/dslogo.png'
 
 const Courses = () => {
     return(<>
@@ -7,19 +10,21 @@ const Courses = () => {
         <Link to="/courses/math" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(207, 255, 51)'}}>
                 <h1>Math</h1>
-                <h4>Mathematics</h4>
+                <img src={mathLogo} alt="math" />
+                
             </div>
         </Link>
         <Link to="/courses/cs" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(53, 65, 229)'}}>
                 <h1>Computer Science</h1>
-                <h4>Programming & Logic based courses</h4>
+                <img src={csLogo} alt="cs" style={{ width: '100px', height: '100px', marginLeft: '27%'}} />
+                
             </div>
         </Link>
         <Link to="ds" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(226, 73, 17)'}}>
                 <h1>Data Science</h1>
-                <h4>Data related courses</h4>
+                <img src={dsLogo} alt="ds" style={{ width: '120px', height: '120px', marginLeft: '23%'}}/>
             </div>
         </Link>
         
--- vite-project/src/pages/tools.jsx
+++ vite-project/src/pages/tools.jsx
@@ -7,19 +7,16 @@ const Tools = () => {
         <Link to="frameworks" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(247, 225, 31)'}}>
                 <h1>Frameworks</h1>
-                <h4>Frames</h4>
             </div>
         </Link>
         <Link to="devtools" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(136, 142, 221)'}}>
-                <h1>Devtools</h1>
-                <h4>dev</h4>
+                <h1>Development Tools</h1>
             </div>
         </Link>
         <Link to="resources" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(96, 247, 31)'}}>
                 <h1>Resources</h1>
-                <h4>helpful</h4>
             </div>
         </Link>  
     </div>
