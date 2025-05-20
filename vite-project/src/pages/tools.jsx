046504f76f93f0d64e726fcc55164522943c769c
Malik <malik.h.jacobs@outlook.com>
2025-05-20 10:14:15 -0400

    added div images for tool divs

--- vite-project/src/pages/tools.jsx
+++ vite-project/src/pages/tools.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
 
+import devLogo from '../assets/devlogo.png';
+import frameLogo from '../assets/framelogo.png';
+import resLogo from '../assets/reslogo.png';
+
 const Tools = () => {
     return(<>
     <h1>Tools</h1>
@@ -7,16 +11,19 @@ const Tools = () => {
         <Link to="frameworks" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(247, 225, 31)'}}>
                 <h1>Frameworks</h1>
+                <img src={frameLogo} alt="fl" style={{ width: '150px', height: '150px', marginLeft: '15%'}}/>
             </div>
         </Link>
         <Link to="devtools" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(136, 142, 221)'}}>
                 <h1>Development Tools</h1>
+                <img src={devLogo} alt="fl" style={{ width: '100px', height: '100px', marginLeft: '26%'}}/>
             </div>
         </Link>
         <Link to="resources" style={{ textDecoration: 'none', color: 'inherit' }}>
             <div className="homediv" style={{backgroundColor: 'rgb(96, 247, 31)'}}>
                 <h1>Resources</h1>
+                <img src={resLogo} alt="fl" style={{ width: '150px', height: '150px', marginLeft: '15%'}}/>
             </div>
         </Link>  
     </div>
