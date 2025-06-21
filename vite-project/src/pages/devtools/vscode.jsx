1fadc0df57654e391853639d461149bf22e66083
Malik <malik.h.jacobs@outlook.com>
2025-06-21 12:00:43 -0400

    added content to vscode page

--- vite-project/src/pages/devtools/vscode.jsx
+++ vite-project/src/pages/devtools/vscode.jsx
@@ -1,17 +1,27 @@
 import YT from '../yt';
 import { Link } from "react-router-dom";
+import vsCode from "../../assets/vsCode.png";
 const Vscode = () =>{
     return(<>
     <div className="tooldiv">
         <h2>Description</h2>
-        <h2>Logo + Link</h2>
-        <h2>Additional sections</h2>
+        <p>Visual Studio Code is the most widely used code editor. Vscode allows development in nearly any programming language or framework. The editor also includes a terminal and downloadable extensions, further enhancing the development process,</p>
+        
+        <a href="https://code.visualstudio.com/" target="_blank" style={{color: 'black', textDecoration: 'none'}}>
+            <h2>Visit site ↓</h2>
+            <img src={vsCode} alt="vsCode" style={{width: '200px'}}/>
+        </a>
+        
         <h2>Videos</h2>
         <div className="video-row">
             <div className="video-wrapper">
-                <YT videoId=""/>
+                <YT videoId="KMxo3T_MTvY"/>
             </div>
         </div>
+
+        <Link to="/tools/devtools" className="L">
+            <button className="divbut3">Back</button>
+        </Link>
     </div>
     </>)
 
