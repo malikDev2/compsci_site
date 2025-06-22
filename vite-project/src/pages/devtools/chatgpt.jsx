4080232a0a4d040bdd33189c84f0e2bd9887bca2
Malik <malik.h.jacobs@outlook.com>
2025-06-22 10:15:22 -0400

    worked on chatgpt and github pages

--- vite-project/src/pages/devtools/chatgpt.jsx
+++ vite-project/src/pages/devtools/chatgpt.jsx
@@ -1,18 +1,28 @@
 import YT from '../yt';
 import { Link } from "react-router-dom";
+import chatGPT from "../../assets/chatGPT.png";
 
 const Chat = () =>{
     return(<>
     <div className="tooldiv">
         <h2>Description</h2>
-        <h2>Logo + Link</h2>
-        <h2>Additional sections</h2>
+        <p>ChatGPT is a generative AI tool developed by OpenAI. ChatGPT assists computer science students by explaining complicated topics, reviewing homework problems, fixing programming bugs, and through many other ways. As AI continues to grow in power, it's capabilites will only expand.</p>
+        
+        <a href="https://chatgpt.com/" target="_blank" style={{color: 'black', textDecoration: 'none'}}>
+            <h2>Visit site ↓</h2>
+            <img src={chatGPT} alt="chatGPT" style={{width: '200px'}}/>
+        </a>
+        
         <h2>Videos</h2>
         <div className="video-row">
             <div className="video-wrapper">
-                <YT videoId=""/>
+                <YT videoId="OGmDr8TLtTo"/>
             </div>
         </div>
+
+        <Link to="/tools/devtools" className="L">
+            <button className="divbut3">Back</button>
+        </Link>
     </div>
     </>)
 
--- vite-project/src/pages/devtools/github.jsx
+++ vite-project/src/pages/devtools/github.jsx
@@ -1,18 +1,28 @@
 import YT from '../yt';
 import { Link } from "react-router-dom";
+import gitHub from "../../assets/gitHub.png";
 
 const Github = () =>{
     return(<>
     <div className="tooldiv">
         <h2>Description</h2>
-        <h2>Logo + Link</h2>
-        <h2>Additional sections</h2>
+        <p>GitHub is a cloud-based platform purposed to store code. It allows users to view their own, as well as any publicly posted repository. Github is used in personal, academic, and professional settings. Github is commonly paired with Git for managing reposotories, however Github can be used by itself. </p>
+        
+        <a href="https://github.com/" target="_blank" style={{color: 'black', textDecoration: 'none'}}>
+            <h2>Visit site ↓</h2>
+            <img src={gitHub} alt="git" style={{width: '200px'}}/>
+        </a>
+        
         <h2>Videos</h2>
         <div className="video-row">
             <div className="video-wrapper">
-                <YT videoId=""/>
+                <YT videoId="a9u2yZvsqHA"/>
             </div>
         </div>
+
+        <Link to="/tools/devtools" className="L">
+            <button className="divbut3">Back</button>
+        </Link>
     </div>
     </>)
 
