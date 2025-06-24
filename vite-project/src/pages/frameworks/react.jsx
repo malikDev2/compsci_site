369f7559d7796641e894c902fe4bb91de646ad38
Malik <malik.h.jacobs@outlook.com>
2025-06-24 10:35:10 -0400

    added content to tkinter and react pages

--- vite-project/src/pages/frameworks/react.jsx
+++ vite-project/src/pages/frameworks/react.jsx
@@ -1,18 +1,28 @@
 import YT from '../yt';
 import { Link } from "react-router-dom";
+import reacT from "../../assets/reacT.png";
 
 const Reac = () =>{
     return(<>
     <div className="tooldiv">
         <h2>Description</h2>
-        <h2>Logo + Link</h2>
-        <h2>Additional sections</h2>
+        <p>React is a frontend framework which vastly improves web development. Key features of React include its reusable component architecture, react hooks such as useState and useEffect, and the react router DOM which vastly simplifies managing webpages. When using react, code is either written in JSX or TSX, which are a mix of Javascript/Typescript and HTML. </p>
+        
+        <a href="https://react.dev/" target="_blank" style={{color: 'black', textDecoration: 'none'}}>
+            <h2>Visit site ↓</h2>
+            <img src={reacT} alt="React Logo" style={{width: '150px'}}/>
+        </a>
+        
         <h2>Videos</h2>
         <div className="video-row">
             <div className="video-wrapper">
-                <YT videoId=""/>
+                <YT videoId="SqcY0GlETPk"/>
             </div>
         </div>
+        <Link to="/tools/frameworks" className="L">
+            <button className="divbut3">Back</button>
+        </Link>
+        
     </div>
     </>)
 
--- vite-project/src/pages/frameworks/tkinter.jsx
+++ vite-project/src/pages/frameworks/tkinter.jsx
@@ -1,18 +1,27 @@
 import YT from '../yt';
 import { Link } from "react-router-dom";
+import tkin from "../../assets/tkin.png";
 
 const Tkinter = () =>{
     return(<>
     <div className="tooldiv">
         <h2>Description</h2>
-        <h2>Logo + Link</h2>
-        <h2>Additional sections</h2>
+        <p>Tkinter is a python GUI used to create basic applications. Tkinter's blocky architecture makes it a great tool for programming beginners.</p>
+        
+        <a href="https://docs.python.org/3/library/tkinter.html" target="_blank" style={{color: 'black', textDecoration: 'none'}}>
+            <h2>Visit site ↓</h2>
+            <img src={tkin} alt="Tkinter Logo" style={{width: '150px'}}/>
+        </a>
+        
         <h2>Videos</h2>
         <div className="video-row">
             <div className="video-wrapper">
-                <YT videoId=""/>
+                <YT videoId="ibf5cx221hk"/>
             </div>
         </div>
+        <Link to="/tools/frameworks" className="L">
+            <button className="divbut3">Back</button>
+        </Link>
     </div>
     </>)
 
