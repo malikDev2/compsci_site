ef9045c27f8bf721ba2ea693d65bd2338b3a069a
Malik <malik.h.jacobs@outlook.com>
2025-06-26 13:05:29 -0400

    completed node page

--- vite-project/src/pages/frameworks/node.jsx
+++ vite-project/src/pages/frameworks/node.jsx
@@ -1,18 +1,31 @@
 import YT from '../yt';
 import { Link } from "react-router-dom";
+import node from "../../assets/nOde.png";
 
 const Node = () =>{
     return(<>
     <div className="tooldiv">
         <h2>Description</h2>
-        <h2>Logo + Link</h2>
-        <h2>Additional sections</h2>
+        <p>Node.js is a runtime environment that executes JavaScript outside of a web browser. Node serves as the backbone for JS web development by managing backend and server-side functionality. Key features of Node include it's asynchronous structure, high performance from google's V8 engine, and the Node Package Manager which provides developers with numeours open-source libraries.   </p>
+        
+        <a href="https://nodejs.org/en" target="_blank" style={{color: 'black', textDecoration: 'none'}}>
+            <h2>Visit site ↓</h2>
+            <img src={node} alt="Node Logo" style={{width: '150px'}}/>
+        </a>
+        
         <h2>Videos</h2>
         <div className="video-row">
             <div className="video-wrapper">
-                <YT videoId=""/>
+                <YT videoId="q-xS25lsN3I"/>
+            </div>
+            <div className="video-wrapper">
+                <YT videoId="TlB_eWDSMt4"/>
             </div>
         </div>
+
+        <Link to="/tools/frameworks" className="L">
+            <button className="divbut3">Back</button>
+        </Link>
     </div>
     </>)
 
