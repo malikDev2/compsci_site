71f3446056f23c86826c693973fbaeef3eba855f
Malik <malik.h.jacobs@outlook.com>
2025-09-01 12:53:22 -0400

    created data science course divs

--- new-vite-app/src/pages/ds.jsx
+++ new-vite-app/src/pages/ds.jsx
@@ -2,8 +2,21 @@ import { Link } from "react-router-dom";
 
 const Ds = () => {
     return(<>
-    <h1>Coming soon...</h1>
-    <h3>Data science courses begin fall '25!</h3>
+    <Link to="data101" className="linkdiv">
+    <div className="language" style={{ backgroundColor: 'rgba(20, 170, 245, 1)', marginTop: "10px" }}>
+        <h2>Data 101</h2>
+        <h4>Introductory Data Science Course</h4>   
+    </div>
+    </Link>
+    <Link to="introds" className="linkdiv">
+    <div className="language" style={{ backgroundColor: 'rgba(20, 245, 39, 1)', marginTop: "10px" }}>
+        <h2>Intro to Data Science </h2>
+        <h4> Data Science overview course covering topics such as Python, SQL, Probability&Statistics, and Machine Learning.</h4>   
+    </div>
+    </Link>
+    
+
+
     <Link to="/courses" className="L">
         <button className="divbut2">Back</button>
     </Link>
